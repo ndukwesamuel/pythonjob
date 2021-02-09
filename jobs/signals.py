c516b378fdd907cf0beeec325d4e46c99d084137
ndukwesamuel <ndukwesamuel23@yahoo.com>
2021-02-09 10:46:25 -0800

    today7

--- jobs/signals.py
+++ jobs/signals.py
@@ -21,19 +21,19 @@ def post_save_create_job(sender, instance, created, **kwargs):
             )
 
 
-post_save.connect(post_save_create_job, sender=User)
-@receiver(post_save, sender=User)
-def post_save_create_job(sender, instance, created, **kwargs):
-    print('sender', sender)
-    print('instance',instance)
-    if created:
-        # group = Group.objects.get(name='customer')
-        # instance.groups.add(group)
-        Company_detail.objects.create(
-            user=instance,
-            Company_name=instance.username,
-            email_address= instance.email
-            )
+# post_save.connect(post_save_create_job, sender=User)
+# @receiver(post_save, sender=User)
+# def post_save_create_job(sender, instance, created, **kwargs):
+#     print('sender', sender)
+#     print('instance',instance)
+#     if created:
+#         # group = Group.objects.get(name='customer')
+#         # instance.groups.add(group)
+#         Company_detail.objects.create(
+#             user=instance,
+#             Company_name=instance.username,
+#             email_address= instance.email
+#             )
 
 
-post_save.connect(post_save_create_job, sender=User)
+# post_save.connect(post_save_create_job, sender=User)
