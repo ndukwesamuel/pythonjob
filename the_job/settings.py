d09ed03e1ca19e413b7d93c36f143636d2e51bd8
ndukwesamuel <ndukwesamuel23@yahoo.com>
2021-02-13 09:23:19 -0800

    the new

--- the_job/settings.py
+++ the_job/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mhs(+om8rwbet(ur*!$*tli46hus9j+t=e1hqh$m8j#sr!nnx!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pyjob.herokuapp.com']
 
